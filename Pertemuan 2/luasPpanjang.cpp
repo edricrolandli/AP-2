ad422919914afd0d6c7b88a1615e4c72ca47a2dc
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 15:53:09 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/luasPpanjang.cpp
+++ Pertemuan 2/luasPpanjang.cpp
@@ -7,8 +7,14 @@ int main(){
     cout << "Masukkan panjang : "; cin >> panjang;
     cout << "Masukkan lebar : "; cin >> lebar;
 
-    luas = panjang * lebar;
-    cout << "Luas = " << luas << endl;
+    luas = panjang * lebar; // ini kayak | L = p x l (di matematika)
+    cout << "Luas = " << luas << endl; 
 
     return 0;
+
+    // simplenya, codingan ini tuh buat hitung luas dari suatu persegi panjang, di mana panjang n lebarnya dari usernya
+    // jadi misal panjang masukin 2
+    // lebar 3
+    // programnya bakal hitung tuh, luas = panjang x lebar (L = 2 x 3)
+    // maka outputnya nanti 6
 }
