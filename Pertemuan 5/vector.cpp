3855eb8b16f1b3e7b62981b3b15b7caca9d062a1
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 21:33:02 +0700

    Pertemuan 5 : Array & Vector

--- Pertemuan 5/vector.cpp
+++ Pertemuan 5/vector.cpp
@@ -7,24 +7,28 @@ using namespace std;
 int main() {
     system("cls");
 
+    // deklarasi vektor dengan tipe data string, "nama"
     vector<string> nama = {"Alya", "Rizky", "Rifki", "Fatih", "Dhafa"};
 
     // for (string karyawan : nama) {
     //     cout << karyawan << endl;
     // }
 
+    // push back untuk add di paling belakang
     nama.push_back("Dani");
     cout << "Setelah menambah data: " << endl;
     for (string karyawan : nama) {
         cout << karyawan << endl;
     }
-
+    
+    // pop back untuk hapus paling belakang
     nama.pop_back();
     cout << "Setelah menghapus data: " << endl;
     for (string karyawan : nama) {
         cout << karyawan << endl;
     }
 
+    // sama sebenarnya hapus juga, tapi ini lebih fleksibel, jadi kita bisa hapus yang di tengah" aja kah, intinya urutan tertentu
     nama.erase(nama.begin() + 3);
     cout << "Setelah menghapus data: " << endl;
     for (string karyawan : nama) {
