fcebf6e107f61af93509bf98cdb6006498c93213
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 17:43:35 +0700

    Pertemuan 6 : Header dan Fungsi

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 using namespace std;
 
-string nama_global = "Ilmu";
+string nama_global = "Ilmu"; 
 
 void nama_variabel (){
     string nama_lokal = "Komputer";
@@ -22,3 +22,11 @@ int main(){
     // coba akses
     // cout << nama_lokal << endl;
 }
+
+// penjelasan di siniiii
+// jadi simplenya codingan ini untuk buktiin pemakaian dan perbedaan dari variable global dan lokal
+// global = bisa diakses di mana aja, termasuk main, ataupun ya dalam fungsi
+// lokal = hanya bisa diakses di fungsi itu aja
+
+// jadi ketika kita coba akses (di line 23), itu pasti error karena gak mungkin variable lokal bisa diakses di main program
+// beda casenya ketika di line 10, itu diakses di fungsi di mana dia di deklarasikan juga
