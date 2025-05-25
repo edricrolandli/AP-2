2f651d7d2c0f2eae81d71b977e9e6ec2d9819ffc
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 16:27:42 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -27,4 +27,15 @@ int main(){
             cout << "Bilangan tidak habis dibagi keduanya";
         }
     }
+
+    // penjelasannya disiniii
+    // jadi simplenya codingan ini untuk ngecek, bilangan yang dimasukkan itu, bisa habis dibagi 5 dan 10, atau hanya 5, atau hanya 10, atau gak habis dibagi 5 ataupun 10
+    // karena ada 4 kondisi, berarti pakai, if-else if-else
+    // nah tapi disini kita bisa pakai nested for, di mana programnya nyari tau dulu di line 11, apakah nilai yang dimasukin bisa dibagi keduanya? (pakai && untuk ensure kedua kondisi true)
+    // kalau misal salah satu aja false, dia bakal ke codingan else, disinilah nested for lagi
+    // ngeceknya satu-satu, dari apakah habis dibagi 5 aja bisa gak? baru ke 10, nah klo 2"nya gabisa, berarti otomatis dong gabisa dibagi dua"nya
+
+    // kenapa gak langsung check bagi 5 dulu? atau bagi 10 dulu? karena ada kemungkinan ketika kita masukkan input 10
+    // berarti bisa dong dibagi keduanya, tapi klo kita ngecek mulai dari (nilai % 5 == 0), outputnya otomatis keluar hanya dibagi 5, padahal harusnya "Bilangan habis dibagi 5 dan 10" kan
+    // gitu ajah
 }
