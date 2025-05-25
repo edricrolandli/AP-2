591ad1ea847a131c43ffee7267a36437a150ce1a
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 18:07:37 +0700

    Pertemuan 7 : Pointer

--- Pertemuan 7/pointer.cpp
+++ Pertemuan 7/pointer.cpp
@@ -1,3 +1,4 @@
+// argh sudah sampai di pointer... semoga bisa dimengerti dengan perumpamaan"
 # include <iostream>
 using namespace std;
 
@@ -17,8 +18,12 @@ int main()
     system("CLS");
 
     // Pointer declaration
-    int number = 35;
-    int *pointer_number = &number;
+    int number = 35; // anggaplah ini rumah nilai 35
+    int *pointer_number = &number; // ini tuh alamatnya
+
+    // Perumpamaan pointer yang diharapkan mudah dimengerti
+    // number dan *pointer_number: isi rumah yang nilainya 35
+    // &number dan pointer_number: alamat rumah yang berisi nilai 35
 
     // cout << "Isi variabel number = " << number << endl;
     // cout << "Alamat dari memori variabel number = " << &number << endl;
@@ -40,6 +45,10 @@ int main()
     // int num[] = {1, 2, 3, 4, 5}; //secara default jika tidak ada disebut index keberapa maka 0
     // int *pointer_num = num;
 
+    // Perumpamaan lagi
+    // num[0] dan *pointer_num : isi rumahnya, tergantung indeks, intinya sama" berisi isi rumah
+    // &num[] dan pointer_num : alamat rumah, tergantung indeks
+
     /* *pointer_num += 5; */
     // cout << "Isi variabel num = " << num[0] << endl;
     // cout << "Alamat dari memori variabel num = " << &num[0] << endl;
@@ -57,10 +66,18 @@ int main()
     // int num2 = 7;
     // penjumlahan(num1, num2);
 
+    // ini penjumlahan biasa kan ya, seperti pertemuan sebelumnya
+
     // cout << num1 << endl;
     // cout << num2 << endl;
 
     // penjumlahan_pointer(&num1, &num2);
+
+    // nah ini bedanya kita pakai pointer, caranya ya intinya inget, pointer pasangannya adalah antara * (isi) dan & (alamat)
+    // jadi awalnya yauda kita input alamatnya, nah di function atas, isinya tuh *, berarti isi dari alamatnya kan
+    // yauda karena dia uda nyimpen alamat dari num1 dan num2, pas mau jumlahin, *a += *b, programnya tinggal ngintip
+    // oh isi dari a (a tuh parameter, aktualnya ya num1 kan, yauda dia ngintip isi dari num1), rupanya 5, dst untuk b
+    // maka hasilnya 12 sama seperti function biasa
 
     // cout << num1 << endl;
     // cout << num2 << endl;
@@ -74,6 +91,16 @@ int main()
     // cout << "Isi variabel pointer_score " << pointer_score << " dan isi variabel yang ditunjuk oleh pointer_score " << *pointer_score << " alamat memori pointer_score " << &pointer_score << endl;
     // cout << "Isi variabel ptr_pointer_score " << ptr_pointer_score << " dan isi variabel yang ditunjuk oleh ptr_pointer_score " << **ptr_pointer_score << " alamat memori pointer_score " << &ptr_pointer_score << endl;
 
+    // makin ribet yah ternyata, tapi mari kita buat versi simple lagi
+    // score : isi rumah, yaitu 4
+    // *pointer_score: alamat rumah dari score, yang isinya 4
+    // **ptr_pointer_score: alamat dari pointer_score 
+    
+    // bayangkannya tuh gini
+    // **ptr_pointer_score -> *pointer_score -> score
+    // jadi kayak nunjuk gitu loh, kesimpulannya yaa secara tak langsung **ptr_pointer_score pada akhirnya bisa akses si score juga (yang isinya 4)
+    // karena panahnya searah kan, bisa nyampe kesana
+    
     // Dynamic Pointer
     int *ptr = new int;
     *ptr = 30;
@@ -82,4 +109,13 @@ int main()
 
     delete ptr;
     cout << "Isi variabel ptr " << *ptr << " dan alamat " << &ptr << endl;
+
+    // row 105, itu new istilahnya create laci baru nih
+    // inget *ptr itu isinya nilai kan, nah di row 106 isinya 30
+
+    // bayangkan sekarang ada laci, terus isinya 30
+
+    // tapi kita bisa nih hapus isi si laci yang isinya 30 tadi, dengan delete
+    // jadi ketika di delete, lacinya tetep ada, inget tetep ada, tapi isinya yang hilang
+    // makanya alamatnya tetap tuh, karena yaa tetep mengarah ke laci, hanya aja isinya kosong, dan bisa diisi lagi
 }
