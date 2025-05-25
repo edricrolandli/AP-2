0433d400b6e10d63b24c125ce4d13607eb136df0
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 17:40:25 +0700

    Pertemuan 6 : Header dan Fungsi

--- Pertemuan 6/belajar_fungsi.cpp
+++ Pertemuan 6/belajar_fungsi.cpp
@@ -10,3 +10,10 @@ int main(){
     sapa(nama_user);
     return 0;
 }
+
+// ini belajar simple sih penggunaan function itu gimana
+// jadi kita bisa tuh masukin inputan user ke dalam function biar fleksibel, contoh mau sapa orang
+// kita gatau kan ya usernya namanya siapa, yauda kita minta masukin
+// nah jadi nanti var nama_user = var aktual
+// dari aktual, dikirimlah ke parameter, nah jadi namanya itu bakal dimasukin ke dalam void (line 5)
+// jadi hasil outputnya bisa tuh "Halo (sesuai nama yang dimasukkan user) dst..." gituuu
