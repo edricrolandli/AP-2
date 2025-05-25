771dffeec823980b07f0a7e1c69e2dcf8169dd4b
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 16:53:16 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -16,4 +16,10 @@ int main(){
     cout << "Kalimat dalam huruf kapital : "  << kalimat << endl;
 
     return 0;
+
+    // codingan ini simplenya biar apapun yang kita masukin, hrufnya jadi besar
+    // kok bisa? caranya kita pakai perulangan, kita cari tau dulu, length dari kata yang dimasukkan itu berapa? misal "saya makan"
+    // berarti ada 10 (spasi dihitung)
+    // nah nanti dia akan ngeubah dari indeks 0, hingga indeks ke 9, menggunakan sintaks toupper
+    // gitu aja sih
 }
