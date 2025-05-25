287db2176b90814391bc867785ef8b45ed22e329
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 17:15:21 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan3.cpp
+++ Pertemuan 4/latihan3.cpp
@@ -23,4 +23,17 @@ int main() {
     }
 
     return 0;
+
+    // ini lebih seru lagi, jadi usernya diminta untuk masukin password dulu, terus sandi yang dimasukkan itu harus unik
+    // contoh : jika abc, maka sandi agen yang bener itu, aababc
+    // jadi kayak berulang gitu
+    // caranya dia nyimpen di var expected
+    // nah itu tuh pakai substr, simplenya kayak ketika kita ada string besar, kita hanya pengen nih sebagian dari string besar itu aja yang kita ambil
+    // contoh, ayam, kita pengen ambil yam nya aja
+    // jadi jadi disini, substr di ambil dari indeks 0, sampai i + 1, ini yang ngebuat programnya bisa ambil lebih dari 1
+    // jadi ketika input "ayam", dia bakal looping dari awal dulu
+    // indeks 0, maka a
+    // indeks 1, maka a + ay = aay
+    // indeks 2, maka a + ay + aya = aayaya
+    // terakhir, indeks 3, aayayaayam
 }
