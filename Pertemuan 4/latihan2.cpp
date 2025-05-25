228b891569dfe8916c9a171d70a4bdf069313cc8
Edric Roland Li <88447297+edricrolandli@users.noreply.github.com>
2025-05-25 16:55:55 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -19,4 +19,11 @@ int main(){
     cout << "Kalimat dalam huruf kapital : "  << kalimat << endl;
 
     return 0;
+
+    // nah kalau yang awalnya kan semua hruf dibuat besar, tapi gimana casenya kita pengen huruf vokal aja? 
+    // kita tambahkan if, jadi kita adjust dikit dari codingan sebelumnya, dia tetep ngecek satu", dari indeks 0 hingga 9, tapi dia cari tau, apakah huruf itu vokal atau engga
+    // contoh, "saya makan", indeks 0, s, bukan aeiou, berarti tetep s
+    // indeks 1, a, nah ke detect tuh, karena memenuhi kondisi if (di mana itu pakai or, jadi salah satu terpenuhi aja dianggap true dan akan jalanin codingan block di dalamnya)
+    // a jadi A
+    // dst hingga outputnya jadi "sAyA mAkAn"
 }
